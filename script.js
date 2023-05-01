049a0043fc35e62f33b40777939c55d36959203d
jesuslucifer <mit.com254@gmail.com>
2023-05-01 17:09:50 +0300

    feat: start task

---
+++ script.js
@@ -0,0 +1,173 @@
+function createMainEn() {
+    const til = "`";
+    const main = document.body;
+    main.innerHTML += `
+    <main class="main">
+    <textarea class="textarea"></textarea>
+    <div class="keyboard">
+    <button class="button">${til}</button>
+    <button class="button">1</button>
+    <button class="button">2</button>
+    <button class="button">3</button>
+    <button class="button">4</button>
+    <button class="button">5</button>
+    <button class="button">6</button>
+    <button class="button">7</button>
+    <button class="button">8</button>
+    <button class="button">9</button>
+    <button class="button">0</button>
+    <button class="button">-</button>
+    <button class="button">=</button>
+    <button class="button backspace">Backspace</button>
+
+    <button class="button tab">Tab</button>
+    <button class="button">q</button>
+    <button class="button">w</button>
+    <button class="button">e</button>
+    <button class="button">r</button>
+    <button class="button">t</button>
+    <button class="button">y</button>
+    <button class="button">u</button>
+    <button class="button">i</button>
+    <button class="button">o</button>
+    <button class="button">p</button>
+    <button class="button">[</button>
+    <button class="button">]</button>
+    <button class="button">/</button>
+    <button class="button">Del</button>
+
+    <button class="button caps">Caps Lock</button>
+    <button class="button">a</button>
+    <button class="button">s</button>
+    <button class="button">d</button>
+    <button class="button">f</button>
+    <button class="button">g</button>
+    <button class="button">h</button>
+    <button class="button">j</button>
+    <button class="button">k</button>
+    <button class="button">l</button>
+    <button class="button">;</button>
+    <button class="button">'</button>
+    <button class="button enter">Enter</button>
+
+    <button class="button shift">Shift</button>
+    <button class="button">z</button>
+    <button class="button">x</button>
+    <button class="button">c</button>
+    <button class="button">v</button>
+    <button class="button">b</button>
+    <button class="button">n</button>
+    <button class="button">m</button>
+    <button class="button">,</button>
+    <button class="button">.</button>
+    <button class="button">/</button>
+    <button class="button shift-right">Shift</button>
+    <button class="button">↑</button>
+    <button>EN</button>
+
+
+    <button class="button ctrl">Ctrl</button>
+    <button class="button win">Win</button>
+    <button class="button alt">Alt</button>
+    <button class="button space">Space</button>
+    <button class="button alt-right">Alt</button>
+    <button class="button ctrl-right">Ctrl</button>
+    <button class="button arrow-right">←</button>
+    <button class="button arrow-down">↓</button>
+    <button class="button arrow-right">→</button>
+    </div>
+    </main>`;
+
+}
+function createMainRu() {
+    const til = "`";
+    const main = document.body;
+    main.innerHTML += `
+    <main class="main">
+    <textarea class="textarea"></textarea>
+    <div class="keyboard">
+    <button class="button">ё</button>
+    <button class="button">1</button>
+    <button class="button">2</button>
+    <button class="button">3</button>
+    <button class="button">4</button>
+    <button class="button">5</button>
+    <button class="button">6</button>
+    <button class="button">7</button>
+    <button class="button">8</button>
+    <button class="button">9</button>
+    <button class="button">0</button>
+    <button class="button">-</button>
+    <button class="button">=</button>
+    <button class="button backspace">Backspace</button>
+
+    <button class="button tab">Tab</button>
+    <button class="button">й</button>
+    <button class="button">ц</button>
+    <button class="button">у</button>
+    <button class="button">к</button>
+    <button class="button">е</button>
+    <button class="button">н</button>
+    <button class="button">г</button>
+    <button class="button">ш</button>
+    <button class="button">щ</button>
+    <button class="button">з</button>
+    <button class="button">х</button>
+    <button class="button">ъ</button>
+    <button class="button">/</button>
+    <button class="button">Del</button>
+
+    <button class="button caps">Caps Lock</button>
+    <button class="button">ф</button>
+    <button class="button">ы</button>
+    <button class="button">в</button>
+    <button class="button">а</button>
+    <button class="button">п</button>
+    <button class="button">h</button>
+    <button class="button">о</button>
+    <button class="button">л</button>
+    <button class="button">д</button>
+    <button class="button">ж</button>
+    <button class="button">э</button>
+    <button class="button enter">Enter</button>
+
+    <button class="button shift">Shift</button>
+    <button class="button">я</button>
+    <button class="button">ч</button>
+    <button class="button">с</button>
+    <button class="button">м</button>
+    <button class="button">и</button>
+    <button class="button">т</button>
+    <button class="button">ь</button>
+    <button class="button">б</button>
+    <button class="button">ю</button>
+    <button class="button">/</button>
+    <button class="button shift-right">Shift</button>
+    <button class="button">↑</button>
+    <button class="button lng">RU</button>
+
+
+    <button class="button ctrl">Ctrl</button>
+    <button class="button win">Win</button>
+    <button class="button alt">Alt</button>
+    <button class="button space">Space</button>
+    <button class="button alt-right">Alt</button>
+    <button class="button ctrl-right">Ctrl</button>
+    <button class="button arrow-right">←</button>
+    <button class="button arrow-down">↓</button>
+    <button class="button arrow-right">→</button>
+    </div>
+    </main>`;
+
+}
+
+createMainEn();
+
+
+let lng = "en";
+
+
+
+
+
+
